6923d80eb98b13b376a771340f971c419c8772a6
Cooke, Tim(e19733) <Tim.Cooke@cmegroup.com>
2016-12-13 17:26:14 +0000

    Day 9 Part 2. It works but by heck it's slow! Could do better.

--- 09/python/cyberspace.py
+++ 09/python/cyberspace.py
@@ -15,8 +15,27 @@ def decompress1(inp):
 def part1():
     line = open(sys.argv[1]).readlines()[0]
     decompressed = decompress1(line)
-    print decompressed
     return len(decompressed)
 
 
+def decompress2(inp):
+    count = 0
+    while inp.find("(") != -1:
+        count += len(inp[:inp.find("(")])
+        instr = inp[inp.find("(") + 1:inp.find(")")].split("x")
+        sectionboundaries = (inp.find(")") + 1, inp.find(")") + 1 + int(instr[0]))
+        decompsection = inp[sectionboundaries[0]:sectionboundaries[1]] * int(instr[1])
+        rest = inp[sectionboundaries[1]:]
+        inp = decompsection + rest
+    count += len(inp)
+    return count
+
+
+def part2():
+    line = open(sys.argv[1]).readlines()[0].replace("\n","")
+    decompressed = decompress2(line)
+    return decompressed
+
+
 print "Part1(): " + str(part1())
+print "Part2(): " + str(part2())
